101356ff1ee63c3116515c2296eadbca5ed2d2ed
unknown <Wayne@.(none)>
2010-08-17 09:07:48 -0400

    Added unit test and fix for sell stop breakout strategy.

--- Platform/ExamplesPlugin/Strategies/ExampleBreakoutStops.cs
+++ Platform/ExamplesPlugin/Strategies/ExampleBreakoutStops.cs
@@ -6,7 +6,10 @@ namespace TickZoom.Examples
 {
 	public class ExampleBreakoutStops : Strategy
 	{	
+<<<<<<< HEAD
 		int tradeSize;
+=======
+>>>>>>> Added unit test and fix for sell stop breakout strategy.
 	    public ExampleBreakoutStops()
 	    {
 	
@@ -14,7 +17,10 @@ namespace TickZoom.Examples
 	
 	    public override void OnInitialize()
 	    {
+<<<<<<< HEAD
 	    	tradeSize = Data.SymbolInfo.Level2LotSize;
+=======
+>>>>>>> Added unit test and fix for sell stop breakout strategy.
 	    }
 
 	    public override bool OnIntervalClose()
@@ -22,7 +28,11 @@ namespace TickZoom.Examples
 			//Example breakout buy code
 			if (this.Position.IsFlat)
 			{
+<<<<<<< HEAD
 				this.Orders.Enter.NextBar.BuyStop(Formula.Highest(Bars.High, 25) + (1 * this.Data.SymbolInfo.MinimumTick), tradeSize);
+=======
+				this.Orders.Enter.NextBar.BuyStop(Formula.Highest(Bars.High, 25) + (1 * this.Data.SymbolInfo.MinimumTick), 1);
+>>>>>>> Added unit test and fix for sell stop breakout strategy.
 			} else if( !this.Position.IsShort) {
 				this.Orders.Exit.NextBar.SellStop(Formula.Lowest(Bars.Low, 25) - (1 * this.Data.SymbolInfo.MinimumTick));
 			}
@@ -30,7 +40,11 @@ namespace TickZoom.Examples
 			//Example breakout sell code
 			if (this.Position.IsFlat)
 			{
+<<<<<<< HEAD
 				this.Orders.Enter.NextBar.SellStop(Formula.Lowest(Bars.Low, 25) - (1 * this.Data.SymbolInfo.MinimumTick), tradeSize);
+=======
+				this.Orders.Enter.NextBar.SellStop(Formula.Lowest(Bars.Low, 25) - (1 * this.Data.SymbolInfo.MinimumTick), 1);
+>>>>>>> Added unit test and fix for sell stop breakout strategy.
 			} else if( !this.Position.IsLong ) {
 				this.Orders.Exit.NextBar.BuyStop(Formula.Highest(Bars.High, 25) + (1 * this.Data.SymbolInfo.MinimumTick));
 			}
