d0e7b6ba91bc42626bec33947ecbe24655d0f1ff
AutoBuild <wayne@tickzoom.com>
2010-11-28 05:28:41 -0500

    Build version 1.1.1.1040.

--- Platform/TickZoomCommon/Properties/AssemblyInfo.cs
+++ Platform/TickZoomCommon/Properties/AssemblyInfo.cs
@@ -41,4 +41,4 @@ using TickZoom.Api;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.0.819")]
+[assembly: AssemblyVersion("1.1.1.1040")]
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.819")]
+[assembly: AssemblyVersion("1.1.1.1040")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
